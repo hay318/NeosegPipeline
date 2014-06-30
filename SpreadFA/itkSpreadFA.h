d7d2c2096a830da6a06930dab0713b29ce5fbadd
marie-cherel2013 <cherel.marie@gmail.com>
2014-06-30 18:47:40 -0400

    ENH: Addition of SpreadFA source code to this repository

---
+++ SpreadFA/itkSpreadFA.h
@@ -0,0 +1,48 @@
+#ifndef __itkSpreadFA_h
+#define __itkSpreadFA_h
+
+namespace itk
+{
+   //namespace Functor
+   //{
+      template< class InputPixelType, class OutputPixelType >
+      class SpreadFA
+      {
+         public:
+            SpreadFA() {}
+            ~SpreadFA() {}
+ 
+            void SetShift(double shift)
+            {
+               m_Shift = shift; 
+            }
+
+            void SetSigma(double sigma)
+            {
+               m_Sigma = sigma; 
+            }
+
+            bool operator!=(const SpreadFA & other) const
+            {
+               return false;
+            }
+ 
+            bool operator==(const SpreadFA & other) const
+            {
+               return !( *this != other );
+            }
+ 
+            inline OutputPixelType operator()(const InputPixelType & x) const
+            { 
+               OutputPixelType result = (erf((x/255-m_Shift)*m_Sigma)+1)/2;
+               return result;
+            }
+
+         private : 
+            double m_Shift;  
+            double m_Sigma; 
+      };
+   //}
+}
+
+#endif
---
+++ SpreadFA/main.cxx
@@ -0,0 +1,51 @@
+// Generate CLP 
+#include "SpreadFACLP.h"
+
+// General Libraries // 
+#include <iostream>
+
+// ITK Librairies //
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkUnaryFunctorImageFilter.h"
+
+// My Specific Librairies // 
+#include "itkSpreadFA.h"
+
+
+int main(int argc, char* argv[])
+{
+   PARSE_ARGS;
+
+   // Types // 
+   typedef itk::Image<float, 3>                    ImageType;
+   typedef itk::ImageFileReader<ImageType>    ReaderType; 
+   typedef itk::ImageFileWriter<ImageType>    WriterType;    
+
+ 
+   // Spread FA Filter //
+   typedef itk::SpreadFA < ImageType::PixelType, ImageType::PixelType >                                                   SpreadFAFunctorType; 
+   typedef itk::UnaryFunctorImageFilter < ImageType, ImageType, SpreadFAFunctorType >                                     SpreadFAFilterType; 
+
+   SpreadFAFilterType::Pointer spreadFAFilter = SpreadFAFilterType::New();
+
+   // Input //
+	ReaderType::Pointer reader = ReaderType::New();
+	reader->SetFileName(input);
+   reader->Update();
+   spreadFAFilter->SetInput(reader->GetOutput()); 
+
+   // Update // 
+   (spreadFAFilter->GetFunctor()).SetShift(shift);
+   (spreadFAFilter->GetFunctor()).SetSigma(sigma);
+   spreadFAFilter->Update(); 
+
+   // Output //
+	WriterType::Pointer writer = WriterType::New(); 
+	writer->SetInput(spreadFAFilter->GetOutput()); 
+	writer->SetFileName(output);
+   writer->SetUseCompression(true); 
+	writer->Update();
+
+}
